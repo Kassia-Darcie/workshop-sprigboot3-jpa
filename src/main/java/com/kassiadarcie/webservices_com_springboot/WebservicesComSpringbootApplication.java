8862f6513ff5ce376d13adb09332bf514e29df66
Kassia Leão <kassiadarcie@gmail.com>
2024-07-09 15:24:07 -0300

    Project created

---
+++ src/main/java/com/kassiadarcie/webservices_com_springboot/WebservicesComSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.kassiadarcie.webservices_com_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesComSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesComSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kassiadarcie/webservices_com_springboot/WebservicesComSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kassiadarcie.webservices_com_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesComSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
