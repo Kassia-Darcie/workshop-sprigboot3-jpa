5f9d8cd5887c63ca1412baaa659d078d84774771
Kassia Leão <kassiadarcie@gmail.com>
2024-07-16 15:47:51 -0300

    Exception handling - update

--- src/main/java/com/kassiadarcie/webservices_com_springboot/services/UserService.java
+++ src/main/java/com/kassiadarcie/webservices_com_springboot/services/UserService.java
@@ -12,6 +12,8 @@ import com.kassiadarcie.webservices_com_springboot.repositories.UserRepository;
 import com.kassiadarcie.webservices_com_springboot.services.exceptions.DatabaseException;
 import com.kassiadarcie.webservices_com_springboot.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -41,9 +43,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
